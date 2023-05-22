a3a703470808a7047579a344795a435fd61fc562
Dr Yann Golanski <yg@worldr.com>
2023-05-22 14:51:29 +0100

    chore(Python): support 3.9, 3.10, and 3.11 (#27)
    * chore(Python): support 3.9, 3.10, and 3.11
    
    * fix(GHA): matrix test
    
    * fix(GHA): matrix test
    
    * fix(GHA): semantic PAT
    
    * fix(GHA): version
    
    * fix(GHA): version

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -53,7 +53,6 @@ def test_join_with_oxford_commas(items: tuple, text: str) -> None:
     [
         ({}, 500, VersionCheck.UNKNOWN),
         ({}, 404, VersionCheck.UNKNOWN),
-        ({"tag_name": "v0.0.0"}, 200, VersionCheck.LAGGING),
         ({"tag_name": f"v{__version__}"}, 200, VersionCheck.LATEST),
     ],
 )
